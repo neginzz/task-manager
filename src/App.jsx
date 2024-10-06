8dbd7dd5064c639d6d42ad82959faa465782b17a
neginz <negin.z566@gmail.com>
2024-10-06 19:42:31 +0330

    [feat] add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import ErrorPage from "./pages/ErrorPage";
+import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="container">
-      <SignupPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ErrorPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
